06bf8db72b1ee969862e2a5a004ec281e8da9560
alstof <alstof@gmail.com>
2018-11-13 10:44:46 -0800

    Add service configuration for DB interactions

--- ECOMCupCake/ECOMCupCake/Startup.cs
+++ ECOMCupCake/ECOMCupCake/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -25,6 +26,11 @@ namespace ECOMCupCake
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<StoreDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:ProdDB"]);
+            });
+
             services.AddMvc();
         }
 
