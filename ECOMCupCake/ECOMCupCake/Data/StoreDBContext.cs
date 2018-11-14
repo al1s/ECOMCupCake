7ec7fbf59c10d5a8fe9a6482c7cd9db4516266a5
alstof <alstof@gmail.com>
2018-11-14 10:57:00 -0800

    Add product table seed

--- ECOMCupCake/ECOMCupCake/Data/StoreDBContext.cs
+++ ECOMCupCake/ECOMCupCake/Data/StoreDBContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ECOMCupCake.Models;
+using Microsoft.EntityFrameworkCore;
 namespace ECOMCupCake.Data
 {
     public class StoreDBContext : DbContext
@@ -6,6 +7,16 @@ namespace ECOMCupCake.Data
         public StoreDBContext(DbContextOptions<StoreDBContext> options)
             : base(options)
         {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product() { ID = 1, Sku = "PP00001", Name = "Pumpkin Pie", Price = 5.87M, Description = "Delicious cake is made with fresh pumpkin from local farms and frosted with whipped cream frosting", Image = "https://images.pexels.com/photos/1055271/pexels-photo-1055271.jpeg" },
+                new Product() { ID = 2, Sku = "RP00002", Name = "Rum Pie", Price = 8.87M, Description = "Delicious cake topped with chocolate, soaked in rum", Image = "https://images.pexels.com/photos/1026123/pexels-photo-1026123.jpeg" },
+                new Product() { ID = 3, Sku = "VL00001", Name = "Vanilla and Lavender", Price = 7.87M, Description = "Vanilla cake with and lavender frosty topping", Image = "https://images.pexels.com/photos/8882/love-heart-purple-dessert.jpg" },
+                new Product() { ID = 4, Sku = "BC00001", Name = "Berry Cake", Price = 4.87M, Description = "Vanilla bean cake paired with assorted berries", Image = "https://images.pexels.com/photos/556829/pexels-photo-556829.jpeg" },
+                new Product() { ID = 5, Sku = "CE00001", Name = "Chocolate Eruption", Price = 9.87M, Description = "Chocolate cake filled with fine crafted milk chocolate", Image = "https://images.pexels.com/photos/360940/pexels-photo-360940.jpeg" }
+            );
         }
     }
 }
