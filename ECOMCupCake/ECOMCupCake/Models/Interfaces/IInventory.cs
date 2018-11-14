ed022171487e65e76d294e37d85070c875582251
alstof <alstof@gmail.com>
2018-11-14 06:06:38 -0800

    Add inventory interface

---
+++ ECOMCupCake/ECOMCupCake/Models/Interfaces/IInventory.cs
@@ -0,0 +1,18 @@
+﻿using ECOMCupCake.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOMCupCake.Models.Interfaces
+{
+    public interface IInventory
+    {
+        Task Create(Inventory inventory);
+        Task<ICollection<Inventory>> GetAll();
+        Task<Inventory> GetById(int id);
+        Task Update(Inventory inventory);
+        Task Delete(int id);
+    }
+}
