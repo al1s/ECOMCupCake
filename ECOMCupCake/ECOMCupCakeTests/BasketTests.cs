7004a0962640a5f7fb325b9b181090cff408f142
alstof <alstof@gmail.com>
2018-11-27 14:20:34 -0800

    Add basket model tests

---
+++ ECOMCupCake/ECOMCupCakeTests/BasketTests.cs
@@ -0,0 +1,41 @@
+﻿using ECOMCupCake.Models;
+using Xunit;
+
+namespace ECOMCupCakeTests
+{
+    public class BasketTests
+    {
+        /// <summary>
+        /// Can set property to an object
+        /// </summary>
+        [Fact]
+        public void Model_OnSetProperty_PropertyIsAssigned()
+        {
+            // arrange
+            Basket basket = new Basket();
+            // act
+            int expectedProductId = 1;
+            int expectedQuantity = 1;
+            basket.ProductID = expectedProductId;
+            basket.Quantity = expectedQuantity;
+            // assert
+            Assert.Equal(expectedProductId, basket.ProductID);
+            Assert.Equal(expectedQuantity, basket.Quantity);
+        }
+
+        /// <summary>
+        /// Can get property to an object
+        /// </summary>
+        [Fact]
+        public void Model_OnGetProperty_PropertyIsEqualToPreviouslySetted()
+        {
+            // arrange
+            Basket basket = new Basket();
+            // act
+            int expectedProductId = 1;
+            basket.ProductID = expectedProductId;
+            // assert
+            Assert.Equal(expectedProductId, basket.ProductID);
+        }
+    }
+}
