3b418a950ffcf1e0554047083e3db0d1e67836d5
alstof <alstof@gmail.com>
2018-12-02 06:52:33 -0800

    Add admin razor pages

---
+++ ECOMCupCake/ECOMCupCake/Pages/Admin/Create.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using ECOMCupCake.Models;
+using ECOMCupCake.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace ECOMCupCake.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly IInventory _inventory;
+
+        public CreateModel(IInventory inventory)
+        {
+            _inventory = inventory;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _inventory.Create(Product);
+            //_context.Products.Add(Product);
+            // await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ECOMCupCake/ECOMCupCake/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using ECOMCupCake.Models;
+using ECOMCupCake.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECOMCupCake.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IInventory _inventory;
+
+
+        public IndexModel(IInventory inventory)
+        {
+            _inventory = inventory;
+        }
+
+        public IList<Product> Product { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var inventory = await _inventory.GetAll(onlyPublished: false);
+            Product = inventory.Items.ToList();
+        }
+    }
+}
